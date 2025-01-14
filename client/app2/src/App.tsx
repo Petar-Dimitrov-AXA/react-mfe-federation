dee90ca30c59d4a0643004a247c2a89ee203c10b
Petar Dimitrov <dimitrovpetar36@gmail.com>
2025-01-14 16:28:26 +0100

    Change app2

--- client/app2/src/App.tsx
+++ client/app2/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+          <h1> Hello From App2 - Petar Dimitrov</h1>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
