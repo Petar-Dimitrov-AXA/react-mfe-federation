2c0ff353b650d532cef8f7998da04ae093f71264
Petar Dimitrov <dimitrovpetar36@gmail.com>
2025-01-14 16:33:08 +0100

    Change application 3

--- client/app3/src/App.tsx
+++ client/app3/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+        <p>Hallo App3</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
